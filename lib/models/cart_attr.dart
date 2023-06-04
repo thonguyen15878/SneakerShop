3658ad6263f6429ffa950f2289e49fb6b8d431e0
thonguyen15878 <thonguyen15878@gmail.com>
2023-06-04 18:11:14 +0700

    add model

---
+++ lib/models/cart_attr.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CartAttr with ChangeNotifier {
+  final String id;
+  final String productId;
+  final String title;
+  final int quantity;
+  final double price;
+  final String imageUrl;
+
+  CartAttr(
+      {required this.id,
+        required this.productId,
+        required this.title,
+        required this.quantity,
+        required this.price,
+        required this.imageUrl});
+}
